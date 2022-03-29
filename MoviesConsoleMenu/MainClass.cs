d72b8542b0ab92d94cde49ab4b718803be4d7273
Vitoria Oliveira <viklic@gmail.com>
2022-03-29 13:52:29 -0400

    Add project files.

---
+++ MoviesConsoleMenu/MainClass.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoviesClassNamespace;
+
+namespace MoviesConsoleMenu
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            MoviesClass mc = new MoviesClassNamespace.MoviesClass();
+
+            Menu();
+        }
+
+        static void Menu()
+        {
+            bool boolResult = false;
+            string strSelectedNumber = "";
+            int intSelectedNumber = 0;
+
+            while (!boolResult)
+            {
+                Console.WriteLine("**********************************");
+                Console.WriteLine("* Welcome to the Movie Database. *");
+                Console.WriteLine("**********************************");
+                Console.WriteLine("Please make your selection from the menu as follows:");
+                Console.WriteLine("1. Choose a movie to watch.");
+                Console.WriteLine("2. Save a new movie in the database");
+                Console.WriteLine("3. Exit this menu.");
+                Console.WriteLine("**********************************");
+                strSelectedNumber = Console.ReadLine();
+
+
+                boolResult = int.TryParse(strSelectedNumber, out intSelectedNumber);
+                if ((intSelectedNumber > 0) && (intSelectedNumber < 4))
+                {
+                    switch(intSelectedNumber)
+                    {
+                        case 1:
+                            Console.WriteLine("You have selected to choose a movie to watch.");
+                            break;
+                        case 2:
+                            Console.WriteLine("You have selected to save a new movie to the database.");
+                            break;
+                        case 3:
+                            Console.WriteLine("You have selected to Exit the menu.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please select a number between 1 to 3 for the menu items.");
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MoviesConsoleMenu/MoviesClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesClassNamespace
+{
+    public class MoviesClass
+    {
+        string _name = "";
+        DateTime _year = DateTime.Now;
+        string _description = "";
+        MovieCategory _movieCategory = MovieCategory.Drama;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                _description = value;
+            }
+        }
+    }
+
+    public enum MovieCategory
+    {
+        Drama,
+        Comedy,
+        Action,
+        Horror
+    }
+}
